6776a277e48e8fd88c93c106b661b42edc32dc01
Ammar Githam <ammargitham786@gmail.com>
2019-03-09 13:42:58 +0900

    remove redux dependency from username.redux test

--- src/redux/tests/username.redux.test.js
+++ src/redux/tests/username.redux.test.js
@@ -1,19 +1,11 @@
-import { createStore } from 'redux';
 import { initialState } from '../../components/App/App';
 import { setUsername } from '../actions/appActions';
 import { appReducer } from '../reducers';
 
 describe('test reduce username', () => {
-  let store;
-  beforeAll(() => {
-    store = createStore(appReducer, initialState);
-  });
-
   test('should set username', async () => {
     const username = 'test';
-    store.dispatch(setUsername(username));
-    const state = await store.getState();
-    // console.log(state);
-    expect(state.username).toEqual(username);
+    const newState = appReducer(initialState, setUsername(username));
+    expect(newState.username).toEqual(username);
   });
 });
